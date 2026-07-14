79b1c75bc65fdfcec5d964a539d5c66199db96ea
home <home@DESKTOP-J7UD23O>
2026-07-14 11:07:32 -0400

    Probar ci en Master

--- CitadoDev.Data/DTOs/OfficeDto.cs
+++ CitadoDev.Data/DTOs/OfficeDto.cs
@@ -1,9 +1,8 @@
 ﻿
 namespace CitadoDev.Data.DTOs
 {
-    public class OfficeDto
+    public class OfficeDto : BaseEntity<int>
     {
-        public required int Id { get; set; }
         public required string RoomNumber { get; set; }
         public required string Floor { get; set; }
         public required string Building { get; set; }
